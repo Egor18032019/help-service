341e43130cb348b8a6c0a0d16f54a9c6def0a23c
egor <goro4@mail.ru>
2024-02-01 10:03:43 +0500

    feat: back

--- src/main/java/org/example/servlet/DummyServlet.java
+++ src/main/java/org/example/servlet/DummyServlet.java
@@ -8,17 +8,5 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 public class DummyServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.getWriter().println("<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <title>Title</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<h1>DummyServlet</h1>\n" +
-                "</body>\n" +
-                "</html>");
-    }
+
 }
