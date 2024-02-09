badc2fc9c6df6f68e8a4ce15d37fc21c73b04435
egor <goro4@mail.ru>
2024-02-09 16:01:29 +0500

    save: local

--- src/main/java/org/example/servlet/HelpServletImpl.java
+++ src/main/java/org/example/servlet/HelpServletImpl.java
@@ -3,7 +3,9 @@ package org.example.servlet;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.context.ApplicationContext;
 import org.example.schemas.HelpRequest;
+import org.example.schemas.HelpResponse;
 import org.example.store.GoodRepository;
+import org.example.utils.Status;
 import org.example.utils.Util;
 
 import javax.servlet.ServletInputStream;
@@ -19,35 +21,29 @@ public class HelpServletImpl extends HttpServlet implements HelpServlet {
     private final GoodRepository goodRepository;
 
 
-    public HelpServletImpl(  ) {
+    public HelpServletImpl() {
 //        goodRepository=new GoodRepositoryImpl();
         ApplicationContext applicationContext = new ApplicationContext();
         goodRepository = applicationContext.getInstance(GoodRepository.class);
     }
-    public HelpServletImpl(GoodRepository repository  ) {
-        goodRepository=repository;
+
+    public HelpServletImpl(GoodRepository repository) {
+        goodRepository = repository;
     }
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json; charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
         String phrase = goodRepository.getRandomPhrase();
         System.out.println("size storage " + goodRepository.getSizeStorage());
-//        String answer = "<html lang=\"ru\">\n" +
-//                "<head>\n" +
-//                "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n" +
-//                "    <title>HELP-SERVICE</title>\n" +
-//                "</head>\n" +
-//                "<body>\n" +
-//                "<h1>" + phrase + "</h1>\n" +
-//                "</body>\n" +
-//                "</html>";
+
 
         HelpRequest helpRequest = new HelpRequest(phrase);
-        String answer =new ObjectMapper().writeValueAsString(helpRequest);
-                PrintWriter writer = response.getWriter();
-        writer.println(answer);
+        String answer = new ObjectMapper().writeValueAsString(helpRequest);
 
+        PrintWriter writer = response.getWriter();
+        writer.println(answer);
         writer.close();
     }
 
@@ -58,6 +54,11 @@ public class HelpServletImpl extends HttpServlet implements HelpServlet {
 
         String text = Util.convertToString(bufferedReader);
         goodRepository.add(text);
-        resp.getWriter().println("ADDED");
+        HelpResponse helpResponse = new HelpResponse(Status.ADDED.toString());
+        String answer = new ObjectMapper().writeValueAsString(helpResponse);
+
+        PrintWriter writer = resp.getWriter();
+        writer.println(answer);
+        writer.close();
     }
 }
