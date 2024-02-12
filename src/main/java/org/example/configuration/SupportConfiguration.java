1ffb3deecbefb04e8e55c6982741471225fdaeb0
egor <goro4@mail.ru>
2024-02-12 14:36:51 +0500

    update: рефакторинг

--- src/main/java/org/example/configuration/SupportConfiguration.java
+++ src/main/java/org/example/configuration/SupportConfiguration.java
@@ -2,7 +2,6 @@ package org.example.configuration;
 
 import org.example.annotation.Configuration;
 import org.example.annotation.Instance;
-import org.example.servlet.AnotherNewController;
 import org.example.servlet.HelpControllerImpl;
 import org.example.store.GoodRepository;
 import org.example.store.GoodRepositoryImpl;
@@ -12,10 +11,6 @@ public class SupportConfiguration {
     @Instance
     public HelpControllerImpl getHelpServlet(GoodRepository getGoodRepository) {
         return new HelpControllerImpl(getGoodRepository);
-    }
-    @Instance
-    public AnotherNewController getAnotherNewServlet(GoodRepository getGoodRepository ) {
-        return new AnotherNewController(getGoodRepository);
     }
 
     @Instance
