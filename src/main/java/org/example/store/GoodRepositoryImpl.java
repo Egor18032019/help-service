aedc083283083e4d9d335cb832e2e5047fa353a3
egor <goro4@mail.ru>
2024-02-12 14:44:50 +0500

    fear: add UUID

--- src/main/java/org/example/store/GoodRepositoryImpl.java
+++ src/main/java/org/example/store/GoodRepositoryImpl.java
@@ -5,13 +5,14 @@ import org.example.utils.Const;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.UUID;
 
 public class GoodRepositoryImpl implements GoodRepository {
-    private static final Map<Integer, String> PHRASE_STORAGE = new HashMap<>();
+    private static final Map<UUID, String> PHRASE_STORAGE = new HashMap<>();
 
     @Override
     public void add(String phrase) {
-        PHRASE_STORAGE.putIfAbsent(PHRASE_STORAGE.size(), phrase);
+        PHRASE_STORAGE.putIfAbsent(UUID.randomUUID(), phrase);
     }
 
     public   String getRandomPhrase() {
@@ -19,9 +20,9 @@ public class GoodRepositoryImpl implements GoodRepository {
 
         if (size == 0) return Const.default_phrase;
 
-        int key = new Random().nextInt(PHRASE_STORAGE.size());
+        int random = new Random().nextInt(PHRASE_STORAGE.size());
+        return PHRASE_STORAGE.values().stream().skip(random).findFirst().orElse(null);
 
-        return PHRASE_STORAGE.get(key);
     }
 
     public   int getSizeStorage() {
