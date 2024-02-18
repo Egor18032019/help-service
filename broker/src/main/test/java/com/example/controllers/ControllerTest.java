e4ce7e8e6b62ee1d805e09aff5dadec24f980731
egor <goro4@mail.ru>
2024-02-18 22:01:11 +0500

    feat: добавил описание

--- broker/src/main/test/java/com/example/controllers/ControllerTest.java
+++ broker/src/main/test/java/com/example/controllers/ControllerTest.java
@@ -1,22 +1,33 @@
 package com.example.controllers;
 
+import com.example.BrokerApp;
+import com.example.store.GoodRepository;
+import com.example.subscriber.SubscriberWithAnnotation;
 import com.example.utils.EndPoint;
+import org.example.subscriber.SubscriberInterface;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(classes = BrokerApp.class)
 @AutoConfigureMockMvc
 public class ControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @SpyBean
+    private GoodRepository repository;
+    @SpyBean
+    private SubscriberWithAnnotation subscriberWithAnnotation;
 
+    @SpyBean
+    private SubscriberInterface subscriberInterface;
     @AfterEach
     public void resetDb() {
         // очистка
